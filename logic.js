c4e6f45432ae7ca78f5919b777aba84db52551f1
Ayman <aalqouqa@gmail.com>
2019-01-15 15:06:09 +0200

    create tests and coding for addTodo function relates #2

--- logic.js
+++ logic.js
@@ -27,7 +27,19 @@ var todoFunctions = {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
       // returns a new array, it should contain todos with the newTodo added to the end.
       // add an id to the newTodo. You can use the generateId function to create an id.
-      // hint: array.concat
+      // hint: array.conca
+      if (newTodo === '' || newTodo === null)
+      return -1;
+    if(!isNaN(newTodo))
+      return -1;
+    let newArray=todos.map(todo=>({...todo}));
+    let obj={};
+    obj.id= todoFunctions.generateId();
+    obj.description=newTodo;
+    obj.done=false;
+    newArray.push(obj);
+    return newArray;
+
     },
     deleteTodo: function(todos, idToDelete) {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
--- test.js
+++ test.js
@@ -1,7 +1,39 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+// addTodo Function test
+let array = [];
+test('Test addTodo function', function(t) {
+  let actual=logic.addTodo(array,'ayman');
+  let expected= [{id:1 , description:'ayman', done:false}]
+  t.deepEqual(actual,expected,'new todo is added to todos array');
+  t.end();
+});
+
+test('Test addTodo function', function(t) {
+  let actual=logic.addTodo(array,'');
+  let expected= -1;
+  t.deepEqual(actual,expected,'add empty description');
+  t.end();
+});
+
+test('Test addTodo function', function(t) {
+  let actual=logic.addTodo(array,null);
+  let expected= -1;
+  t.deepEqual(actual,expected,'add null description');
+  t.end();
+});
+
+test('Test addTodo function', function(t) {
+  let actual=logic.addTodo(array,2);
+  let expected= -1;
+  t.deepEqual(actual,expected,'add number description');
+  t.end();
+});
+
+test('Test addTodo function', function(t) {
+  let actual=logic.addTodo(array,' ');
+  let expected= -1;
+  t.deepEqual(actual,expected,'add space description');
   t.end();
 });
